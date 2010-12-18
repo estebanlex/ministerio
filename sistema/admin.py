683beb2976cab3d1da9a7a6a08572ada736c3a09
“estebanlex” <estebanlex23@hotmail.com>
2010-12-18 17:45:53 -0300

    lineas

--- sistema/admin.py
+++ sistema/admin.py
@@ -2,7 +2,7 @@ from ministerio.sistema.models import congregacion
 from django.contrib import admin
 
 class congregacionAdmin(admin.ModelAdmin):
-     list_display = ('nombre_cong', 'direccion_cong','comuna_cong')
+     list_display = ('nombre_cong', 'direccion_cong','comuna_cong','ciudad_cong')
 #     exclude = ('id_cong')
 #     ordering = ('nombre_cong')
 #     search_fields = ('nombre_cong')    
